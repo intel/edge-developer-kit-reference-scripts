15f7802d841ef1bc8b35f9720dca6373c081b42f
laykuanloon <kuan.loon.lay@intel.com>
2024-05-08 09:55:39 +0800

    NetSec Openvino bert (#95)
    * NetSec Openvino bert
    
    Signed-off-by: Lay, Kuan Loon <kuan.loon.lay@intel.com>
    
    * Update setup_ov.md
    
    add trademark OpenVINO™ on title
    
    ---------
    
    Signed-off-by: Lay, Kuan Loon <kuan.loon.lay@intel.com>
    Co-authored-by: gooishin <100290970+gooishin@users.noreply.github.com>

---
+++ usecases/netsec/openvino-bert/setup_ov.md
@@ -0,0 +1,43 @@
+# Openvino™
+Benchmark OpenVINO™  bert model
+
+## Steps
+### 1. Setup OpenVINO™
+Setup OpenVINO™
+```bash
+./setup_ov.sh
+```
+Installation is completed when you see this message:
+> ✓ OpenVINO™  configured
+
+### 2. Benchmark 
+
+Benchmark bert-base-cased model (FP32)
+```bash
+#Python virtual environment to testing bert openvino model
+source bert_ov_venv/bin/activate
+
+#CPU
+numactl -C 0 benchmark_app -m models/bert-base-cased.xml -d CPU -hint latency -shape "[1, 512]"
+#GPU
+numactl -C 0 benchmark_app -m models/bert-base-cased.xml -d GPU -hint latency -shape "[1, 512]"
+
+#Deactivate virtual environment
+deactivate
+
+```
+
+Benchmark quantized bert-base-cased model (INT8)
+```bash
+
+#Python virtual environment to testing bert openvino model
+source bert_ov_venv/bin/activate
+
+#CPU
+numactl -C 0 benchmark_app -m models/quantized_bert_base_cased.xml -d CPU -hint latency -shape "[1, 512]"
+#GPU
+numactl -C 0 benchmark_app -m models/quantized_bert_base_cased.xml -d GPU -hint latency -shape "[1, 512]"
+
+#Deactivate virtual environment
+deactivate
+```
---
+++ usecases/netsec/openvino-bert/torch-to-ov.py
@@ -0,0 +1,94 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import sys
+import time
+from pathlib import Path
+from zipfile import ZipFile
+from typing import Iterable
+from typing import Any
+
+import datasets
+import numpy as np
+import nncf
+from nncf.parameters import ModelType
+import openvino as ov
+import torch
+from transformers import BertForSequenceClassification, BertTokenizer
+
+
+MODEL_DIR = "models"
+os.makedirs(MODEL_DIR, exist_ok=True)
+
+MAX_SEQ_LENGTH = 512
+
+
+def load_model(inputs, input_info):
+    ir_model_xml = Path(MODEL_DIR) / "bert-base-cased.xml"
+    core = ov.Core()
+
+    torch_model = BertForSequenceClassification.from_pretrained('bert-base-cased')
+    torch_model.eval
+
+    # Convert the PyTorch model to OpenVINO IR FP32.
+    if not ir_model_xml.exists():
+        model = ov.convert_model(torch_model, example_input=inputs, input=input_info)
+        ov.save_model(model, str(ir_model_xml))
+    else:
+        model = core.read_model(ir_model_xml)
+
+    return model
+
+
+def create_data_source():
+    raw_dataset = datasets.load_dataset('glue', 'mrpc', split='validation')
+    tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+
+    def _preprocess_fn(examples):
+        texts = (examples['sentence1'], examples['sentence2'])
+        result = tokenizer(*texts, padding='max_length', max_length=MAX_SEQ_LENGTH, truncation=True)
+        result['labels'] = examples['label']
+        return result
+    processed_dataset = raw_dataset.map(_preprocess_fn, batched=True, batch_size=1)
+
+    return processed_dataset
+
+
+def nncf_quantize(model, inputs):
+    INPUT_NAMES = [key for key in inputs.keys()]
+    data_source = create_data_source()
+
+    def transform_fn(data_item):
+        """
+        Extract the model's input from the data item.
+        The data item here is the data item that is returned from the data source per iteration.
+        This function should be passed when the data item cannot be used as model's input.
+        """
+        inputs = {
+            name: np.asarray([data_item[name]], dtype=np.int64) for name in INPUT_NAMES
+        }
+        return inputs
+
+    calibration_dataset = nncf.Dataset(data_source, transform_fn)
+    # Quantize the model. By specifying model_type, we specify additional transformer patterns in the model.
+    quantized_model = nncf.quantize(model, calibration_dataset,
+                                    model_type=ModelType.TRANSFORMER)
+
+
+    compressed_model_xml = Path(MODEL_DIR) / "quantized_bert_base_cased.xml"
+    ov.save_model(quantized_model, compressed_model_xml)
+
+
+if __name__ == '__main__':
+    input_shape = ov.PartialShape([1, 512])
+    input_info = [("input_ids", input_shape, np.int64),("attention_mask", input_shape, np.int64),("token_type_ids", input_shape, np.int64)]
+    default_input = torch.ones(1, MAX_SEQ_LENGTH, dtype=torch.int64)
+    inputs = {
+        "input_ids": default_input,
+        "attention_mask": default_input,
+        "token_type_ids": default_input,
+    }
+
+    model = load_model(inputs, input_info)
+    quantized_model = nncf_quantize(model, inputs)
