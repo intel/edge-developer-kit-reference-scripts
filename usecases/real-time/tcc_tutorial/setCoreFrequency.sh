1073c78ef9b56314f5409a57fa6854f4da3415a5
Markus Schweikhardt <Markus.Schweikhardt@intel.com>
2025-02-06 02:01:44 +0100

    Update of the documentation and ensure that code fits to the configuration discussed in the documentation (#350)

--- usecases/real-time/tcc_tutorial/setCoreFrequency.sh
+++ usecases/real-time/tcc_tutorial/setCoreFrequency.sh
@@ -74,59 +74,68 @@ basefrequency() {
 
 # Function for rt-boost
 rt_boost() {
-    local selected_cpus=("$1")
-    local desired_min_freq=$2
-    local desired_max_freq=$3
+    local selected_cpus=("$@")
+    local desired_min_freq=${selected_cpus[-2]}
+    local desired_max_freq=${selected_cpus[-1]}
+    unset "selected_cpus[-1]"
+    unset "selected_cpus[-1]"
 
     # Iterate through all CPU directories
     for cpu_dir in /sys/devices/system/cpu/cpu*/cpufreq; 
     do
         cpu_id=$(basename "$(dirname "$cpu_dir")" | sed 's/cpu//')
 
-        # Check if the cpuinfo_min_freq file exists
-        if [ -f "$cpu_dir/cpuinfo_min_freq" ]; then
-            # Read the cpuinfo_min_freq value
-            min_freq=$(cat "$cpu_dir/cpuinfo_min_freq")
-            
-            # Write the cpuinfo_min_freq value to scaling_min_freq
-            sudo bash -c "echo $min_freq > $cpu_dir/scaling_min_freq"
-        else
-            echo "cpuinfo_min_freq file not found in $cpu_dir"
-        fi
-
-        # Check if the base_frequency file exists
-        if [ -f "$cpu_dir/base_frequency" ]; then
-            # Read the base frequency value
-            base_freq=$(cat "$cpu_dir/base_frequency")
-            
-            # Write the base frequency value to scaling_max_freq
-            sudo bash -c "echo $base_freq > $cpu_dir/scaling_max_freq"
-        else
-            echo "base_frequency file not found in $cpu_dir"
-        fi
-
-        # Check if the energy_performance_preference file exists
-        if [ -f "$cpu_dir/energy_performance_preference" ]; then
-            # Set energy_performance_preference to "power" by default
-            sudo bash -c "echo balance_power > $cpu_dir/energy_performance_preference"
-        else
-            echo "energy_performance_preference file not found in $cpu_dir"
-        fi
+        # Check if the current CPU is in the selected CPUs list
+        cpu_selected=false
         for selected_cpu in "${selected_cpus[@]}"; do
-            # Check if the current CPU is in the selected CPUs list
-            if [[ "$selected_cpu" == "$cpu_id" ]]; then
-                    # Set energy_performance_preference to "performance"
-                sudo bash -c "echo performance > $cpu_dir/energy_performance_preference"
-            
-                # Set scaling_min_freq and scaling_max_freq to the desired values
-                sudo bash -c "echo $desired_min_freq > $cpu_dir/scaling_min_freq"
-                sudo bash -c "echo $desired_max_freq > $cpu_dir/scaling_max_freq"
-            
-                echo "Set CPU $cpu_id to performance mode with min freq $desired_min_freq and max freq $desired_max_freq"
-            else
-                echo "Set CPU $cpu_id to power mode with min freq $min_freq and max freq $base_freq"
+            if [ "$selected_cpu" == "$cpu_id" ]; then
+                cpu_selected=true
+                break
             fi
         done
+
+        if [ "$cpu_selected" == true ]; then
+            # Set energy_performance_preference to "performance"
+            sudo bash -c "echo performance > $cpu_dir/energy_performance_preference"
+        
+            # Set scaling_min_freq and scaling_max_freq to the desired values
+            sudo bash -c "echo $desired_min_freq > $cpu_dir/scaling_min_freq"
+            sudo bash -c "echo $desired_max_freq > $cpu_dir/scaling_max_freq"
+        
+            echo "Set CPU $cpu_id to performance mode with min freq $desired_min_freq and max freq $desired_max_freq"
+        else
+            # Check if the cpuinfo_min_freq file exists
+            if [ -f "$cpu_dir/cpuinfo_min_freq" ]; then
+                # Read the cpuinfo_min_freq value
+                min_freq=$(cat "$cpu_dir/cpuinfo_min_freq")
+                
+                # Write the cpuinfo_min_freq value to scaling_min_freq
+                sudo bash -c "echo $min_freq > $cpu_dir/scaling_min_freq"
+            else
+                echo "cpuinfo_min_freq file not found in $cpu_dir"
+            fi
+
+            # Check if the base_frequency file exists
+            if [ -f "$cpu_dir/base_frequency" ]; then
+                # Read the base frequency value
+                base_freq=$(cat "$cpu_dir/base_frequency")
+                
+                # Write the base frequency value to scaling_max_freq
+                sudo bash -c "echo $base_freq > $cpu_dir/scaling_max_freq"
+            else
+                echo "base_frequency file not found in $cpu_dir"
+            fi
+
+            # Check if the energy_performance_preference file exists
+            if [ -f "$cpu_dir/energy_performance_preference" ]; then
+                # Set energy_performance_preference to "power" by default
+                sudo bash -c "echo balance_power > $cpu_dir/energy_performance_preference"
+            else
+                echo "energy_performance_preference file not found in $cpu_dir"
+            fi
+
+            echo "Set CPU $cpu_id to power mode with min freq $min_freq and max freq $base_freq"
+        fi
     done
 }
 
@@ -134,15 +143,14 @@ rt_boost() {
 if [ "$1" == "basefrequency" ]; then
     basefrequency
 elif [ "$1" == "rt-boost" ]; then
-#    echo "Usage: $0 rt-boost"
-#    if [ $# -lt 4 ]; then
-#        echo "Usage: $0 rt-boost <cpu_list> <min_freq> <max_freq>"
-#        echo "Example: $0 rt-boost '0 2' 2000000 3000000"
-#        exit 1
-    selected_cpus=(3) #($2)
-    desired_min_freq=3100000 #$3
-    desired_max_freq=3100000 #$4
-    rt_boost "${selected_cpus[@]}" $desired_min_freq $desired_max_freq
+    if [ $# -lt 4 ]; then
+        echo "Usage: $0 rt-boost <cpu_list> <min_freq> <max_freq>"
+        echo "Example: $0 rt-boost '0 2' 2000000 3000000"
+        exit 1
+    fi
+    IFS=' ' read -r -a selected_cpus <<< "$2"
+    selected_cpus+=("$3" "$4")
+    rt_boost "${selected_cpus[@]}"
 else
     echo "Usage: $0 {basefrequency|rt-boost}"
     exit 1
